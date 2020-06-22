67d30a82fdc8cfa328cfed6c04f1a3d501992486
wasabi1998 <cavalier0087@163.com>
2020-06-22 22:48:59 +0800

    日志器配置

--- blog/settings.py
+++ blog/settings.py
@@ -162,3 +162,79 @@ CACHES = {
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 SESSION_CACHE_ALIAS = 'session'
 SESSION_COOKIE_AGE = 86400
+
+# 日志配置
+# import os
+# BASE_LOG_DIR = os.path.join(BASE_DIR , "log")   # logging日志文件配置的位置
+LOGGING = {
+    'version': 1,  # 保留字段
+    'disable_existing_loggers': False,      # 不要禁用已经存在的logger实例
+    'formatters': {     # 定义三种日志显示的格式
+        'standard': {
+            'format': '[%(levelname)s][%(asctime)s][%(threadName)s:%(thread)d][task_id:%(name)s][%(module)s][%(filename)s:%(lineno)d]'
+                      '[%(message)s]'
+        },
+        'verbose': {
+            'format': '[%(levelname)s][%(asctime)s][%(module)s][%(filename)s:%(lineno)d][%(message)s]'
+        },
+        'simple': {
+            'format': '[%(levelname)s][%(filename)s:%(lineno)d][%(message)s]'
+        },
+        'collect': {
+            'format': '[%(levelname)s][%(message)s]'
+        }
+    },
+    'filters': {    # 定义一个过滤规则
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',  # ():括弧代表在任意时刻都执行
+        },
+    },
+    'handlers': {  # 日志流的处理方式
+        'console': {    # 把日志打印到终端时设置的参数
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],  # 只有在Django debug为True时才在屏幕打印日志
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'default': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',  # 保存到文件，自动切
+            'filename': os.path.join(BASE_DIR, "logs/default.log"),  # 日志文件
+            'maxBytes': 1024 * 1024 * 500,  # 日志大小 500M
+            'backupCount': 3,  #
+            'formatter': 'standard',
+            'encoding': 'utf-8',    # 设置编码是为了防止中文编码混乱
+        },
+        'error': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',  # 保存到文件，自动切
+            'filename': os.path.join(BASE_DIR, "logs/error.log"),  # 日志文件
+            'maxBytes': 1024 * 1024 * 500,  # 日志大小 500M
+            'backupCount': 5,
+            'formatter': 'standard',
+            'encoding': 'utf-8',
+        },
+        'collect': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',  # 保存到文件，自动切换文件保存
+            'filename': os.path.join(BASE_DIR, "logs/collect.log"),
+            'maxBytes': 1024 * 1024 * 500,  # 日志大小 500M
+            'backupCount': 5,
+            'formatter': 'collect',
+            'encoding': "utf-8"
+        }
+    },
+    'loggers': {
+       # 默认的logger应用如下配置
+        'djangologger': {
+            'handlers': ['default', 'console', 'error'],  # 上线之后可以把'console'移除
+            'level': 'DEBUG',
+            'propagate': True,      # propagate：是否向父级logger实例传递日志信息
+        },
+        # 名为 'collect'的logger还单独处理
+        'collect': {
+            'handlers': ['console', 'collect'],
+            'level': 'INFO',
+        }
+    },
+}
--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#1.导入系统的logging
+import logging
+#2.创建(获取)日志器
+logger = logging.getLogger('djangologger')
+#3.使用日志器记录日志信息
+from django.http import HttpResponse
+def log(request):
+    logger.info('info')
+    return HttpResponse('test')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',log),
 ]
+
